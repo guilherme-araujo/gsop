d8be1576d4b3317faf2b2244930be6b4da482f5d
Guilherme Fernandes de Araujo <guilherme.araujo@imd.ufrn.br>
2017-12-19 14:52:14 -0300

    Commit inicial

---
+++ gsop/src/main/java/br/ufrn/imd/bioinfo/gsop/App.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.bioinfo.gsop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
